2a14e6c3417c3a73224c9858681610b5f8efd000
gavinwea414-a11y <gavinwea414@gmail.com>
2026-05-07 11:06:38 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,32 @@
-const CACHE_NAME = "esp-switch-pwa-v2";
-
-const CORE_ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  const requestUrl = new URL(event.request.url);
-
-  const isSameOrigin = requestUrl.origin === self.location.origin;
-  if (!isSameOrigin) return;
-
-  const isHtmlRequest =
-    event.request.mode === "navigate" ||
-    requestUrl.pathname.endsWith("/") ||
-    requestUrl.pathname.endsWith("/index.html");
-
-  if (isHtmlRequest) {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const responseClone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", responseClone));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request).then(response => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
-        return response;
-      });
-    })
-  );
-});
+diff --git a/service-worker.js b/service-worker.js
+index ed7aa86c3afe1d59c0f33e8ad8793f23ac270224..5bfdf1571f157396f7a61d09aeae978d715e90b4 100644
+--- a/service-worker.js
++++ b/service-worker.js
+@@ -1,26 +1,26 @@
+-const CACHE_NAME = "esp-switch-pwa-v2";
++const CACHE_NAME = "esp-switch-pwa-v3";
+ 
+ const CORE_ASSETS = [
+   "./",
+   "./index.html",
+   "./manifest.json",
+   "./icon-192.png",
+   "./icon-512.png"
+ ];
+ 
+ self.addEventListener("install", event => {
+   event.waitUntil(
+     caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+   );
+   self.skipWaiting();
+ });
+ 
+ self.addEventListener("activate", event => {
+   event.waitUntil(
+     caches.keys().then(keys =>
+       Promise.all(
+         keys.map(key => {
+           if (key !== CACHE_NAME) {
+             return caches.delete(key);
+           }
+         })
